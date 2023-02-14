918678704070cd3b46db24e5d115011af2cc22f5
Chris Donohue <christopher.donohue@gmail.com>
2023-02-14 12:17:51 -0500

    working with database

--- app.py
+++ app.py
@@ -1,48 +1,88 @@
 import cv2
+import queue
+import threading
 import openalpr
-import shutil
 import logging
+import shutil
+import sqlite3
+import numpy as np
+from datetime import datetime
+import uuid
+import os
 
-logging.basicConfig(level=logging.INFO,
+database_file = "images.db"
+
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                     handlers=[logging.StreamHandler()])
 
-def preprocess_image(image_path):
-    # Load the image
-    image = cv2.imread(image_path)
+def add_image_to_database(database_file, image, plate):
+    # Convert the image to a binary string
+    image_bytes = cv2.imencode('.jpg', image)[1].tobytes()
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Generate a random UUID
+    unique_id = str(uuid.uuid4())
 
-    # Apply Gaussian blur to the image
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    # Create a filename with the UUID
+    filename = f"{plate}_{unique_id}.jpg"
 
-    # Apply adaptive threshold to the image
-    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    cv2.imwrite(os.path.join('images', filename), image)
 
-    # Save the preprocessed image
-    cv2.imwrite(image_path, thresh)
-    
+    # Connect to the database
+    conn = sqlite3.connect(database_file)
+    c = conn.cursor()
+
+    # Create the table if it does not already exist
+    c.execute('CREATE TABLE IF NOT EXISTS images (time Text, plate TEXT, filename TEXT)')
+
+    # Insert the image into the database
+    time = datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
+    c.execute('INSERT INTO images VALUES (?, ?, ?)', (time, plate, filename))
+
+    # Commit the transaction and close the connection
+    conn.commit()
+    conn.close()
+
+# Set up the video capture from an RTSP stream
+rtsp_url = "rtsp://192.168.1.202:554/11"
+cap = cv2.VideoCapture(rtsp_url)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+# Set up a thread-safe queue to hold the captured frames
+frame_queue = queue.Queue(maxsize=20)
+
+# Define a function to capture frames and add them to the queue
+def capture_frames():
+    global cap
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to capture frame from stream")
+            cap = cv2.VideoCapture(rtsp_url)
+            cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+            continue
+     
+        # Add the current frame to the queue
+        try:
+            frame_queue.put_nowait(frame)
+        except queue.Full:
+            pass
+
+        # Wait for one second before capturing the next frame
+        cv2.waitKey(1000)
 
 def copy_image(src_file, dst_file):
     shutil.copy(src_file, dst_file)
     logging.info(f"Image file copied from {src_file} to {dst_file}")
 
-def capture_frame(url):
-    cap = cv2.VideoCapture(url)
-    ret, frame = cap.read()
-    if ret:
-        cv2.imwrite('frame.jpg', frame)
-        logging.info('Captured frame successfully')
-    else:
-        logging.error('Failed to capture frame')
-
-def recognize_plate(image_path, region='us'):
+def recognize_plate(frame, region='us'):
     # Initialize the OpenALPR library
     alpr = openalpr.Alpr(region, "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
 
     # Read the license plate numbers from the image
-    results = alpr.recognize_file(image_path)
+    ret, enc = cv2.imencode("*.bmp", frame)
+    results = alpr.recognize_array(bytes(bytearray(enc)))
 
     # Iterate through the results and log the license plate numbers
     for plate in results["results"]:
@@ -50,13 +90,24 @@ def recognize_plate(image_path, region='us'):
         logging.info("Confidence: " + str(plate["confidence"]))
 
         if plate["confidence"] > 80:
-            copy_image(image_path, plate['plate'] + '.jpg')
+            add_image_to_database(database_file, frame, plate['plate'])
 
     # Unload the OpenALPR library
     alpr.unload()
 
-if __name__ == '__main__':
-    while True:
-        capture_frame('rtsp://192.168.1.202:554/11')
-        preprocess_image('frame.jpg')
-        recognize_plate("frame.jpg")
+
+# Start a new thread to capture frames and add them to the queue
+thread = threading.Thread(target=capture_frames)
+thread.daemon = True
+thread.start()
+
+# Process frames from the queue
+while True:
+    # Get the next frame from the queue
+    frame = frame_queue.get()
+
+    recognize_plate(frame)
+
+    logging.debug('queue size ' + str(frame_queue.qsize()))
+
+cap.release()
